cfcf81428d247340a381d2bdafdd7c48ddcd9e24
tavisca-idrisw <idrisw@tavisca.com>
2015-07-18 00:59:43 +0530

    WCF work in progress....

---
+++ WCFEmployee/WCFEmployee.Service/WCFEmployee.Service/WCFEmployee.Service/ServiceContracts/IEmployeeManagementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace WCFEmployee.Services
+{
+    public interface IEmployeeManagementService
+    {
+        [OperationContract(Name = "SearchById")]
+        Employee Get(int Id);
+
+        [OperationContract]
+        [FaultContract(typeof(CustomTypeException))]
+        List<Employee> GetEmployees();
+    }
+}
