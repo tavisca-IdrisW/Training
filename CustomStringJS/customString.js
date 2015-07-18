c84810f98d6712dfac667b917d3c3d4ec308f4bd
IdrisOPT <idris@optcentral.com>
2015-07-19 01:41:05 +0530

    Adding custom string funcstons for review.

---
+++ CustomStringJS/customString.js
@@ -0,0 +1,7 @@
+String.prototype.Substr = function (fromIndex, toIndex) {
+  return this.substr(fromIndex, toIndex);
+};
+
+String.prototype.Concat = function (appnedString) {
+  return this + appnedString;
+};
