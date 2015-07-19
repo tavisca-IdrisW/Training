c647f5b998863bc3ad6a323b446b4a9a46fc0938
IdrisOPT <idris@optcentral.com>
2015-07-19 22:04:12 +0530

    Adding WCF Employee Assignment for review.

---
+++ EmployeeWCF/Tavisca.EmployeeServices/Tavisca.EmployeeServices/ServiceContracts/ICreateEmployeeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+
+
+namespace Tavisca.EmployeeServices
+{
+    [ServiceContract]
+    public interface ICreateEmployeeService
+    {
+        [OperationContract]
+        [FaultContract(typeof(CustomTypeException))]
+        Employee Create(Employee employee);
+
+        [OperationContract]
+        [FaultContract(typeof(CustomTypeException))]
+        Remark AddRemark(string employeeId, Remark remark);
+    }
+}
---
+++ EmployeeWCF/Tavisca.EmployeeServices/Tavisca.EmployeeServices/ServiceContracts/IEmployeeManagementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Tavisca.EmployeeServices
+{
+    public interface IEmployeeManagementService
+    {
+        [OperationContract(Name = "SearchById")]
+        Employee Get(int Id);
+
+        [OperationContract]
+        [FaultContract(typeof(CustomTypeException))]
+        List<Employee> GetEmployees();
+    }
+}
