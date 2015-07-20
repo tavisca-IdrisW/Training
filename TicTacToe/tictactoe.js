6be78094afa928a5b8701c91d1f3667392c04854
tavisca-idrisw <idrisw@tavisca.com>
2015-07-20 18:15:40 +0530

    Added CSS.

--- TicTacToe/tictactoe.js
+++ TicTacToe/tictactoe.js
@@ -5,22 +5,7 @@
       moves,
       turn = "X",
       oldOnload,
-
-  /*
-   * Winable sums... in 2^cell# form.
-   *     273                 84
-   *        \               /
-   *          1 |   2 |   4  = 7
-   *       -----+-----+-----
-   *          8 |  16 |  32  = 56
-   *       -----+-----+-----
-   *         64 | 128 | 256  = 448
-   *       =================
-   *         73   146   292
-   *
-   */
-
-  wins = [7, 56, 448, 73, 146, 292, 273, 84];
+      wins = [7, 56, 448, 73, 146, 292, 273, 84];
 
   startNewGame = function () {
    var i;
@@ -79,33 +64,28 @@
         cell,
         parent;
         indicator = 1,
-        board = document.createElement("table"),
+        board = document.getElementById("board"),
         board.border = 1;
 
-      for (var i = 1; i <= 3; i += 1) {  
+    for (var i = 1; i <= 3; i += 1) {  
 
-        var row = document.createElement("tr");
-        
-        board.appendChild(row);
-       
-        for (var j = 1; j <= 3; j += 1) {
+      var row = document.createElement("tr");
+      
+      board.appendChild(row);
+     
+      for (var j = 1; j <= 3; j += 1) {
 
-          var cell = document.createElement("td");
-          
-          cell.width = cell.height = 25;
-          cell.align = cell.valign = 'center';
-          cell.indicator = indicator;
-          cell.onclick = this.set;
-          cell.appendChild(document.createTextNode(""));
-          row.appendChild(cell);
-          block.push(cell);
-          indicator += indicator;
-        }
+        var cell = document.createElement("td");
+        cell.addClass('cell');
+        cell.indicator = indicator;
+        cell.onclick = this.set;
+        cell.appendChild(document.createTextNode(""));
+        row.appendChild(cell);
+        block.push(cell);
+        indicator += indicator;
       }
-
-      parent = document.getElementById("tictactoe") || document.body;
-      parent.appendChild(board);
-      startNewGame();
+    }
+    startNewGame();
   };
 
 
