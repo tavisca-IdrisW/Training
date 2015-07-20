40ce2d80c6c22db1643de126be7858fb9ad3e77d
tavisca-idrisw <idrisw@tavisca.com>
2015-07-20 20:37:32 +0530

    Indented code.

--- BouncingBall/Views/ballView.js
+++ BouncingBall/Views/ballView.js
@@ -1,31 +1,29 @@
 window.bouncingBall = window.bouncingBall || {
-   minXCoordinate: 15,
-   maxXCoordinate: window.innerWidth-40,
-   minYCoordinate: 15,
-   maxYCoordinate: window.innerHeight-45,
-   addX: 20,
-   addY: 15
-};
+ minXCoordinate: 15,
+ maxXCoordinate: window.innerWidth-40,
+ minYCoordinate: 15,
+ maxYCoordinate: window.innerHeight-45,
+ addX: 20,
+ addY: 15
+}
 
-
-
- bouncingBall.start = function() {
-  bouncingBall.ball = document.getElementById("ball");
-  bouncingBall.ball.style.top = Math.random()*(bouncingBall.maxYCoordinate)+'px'; // Starting Position 
-  bouncingBall.ball.style.left = Math.random()*(bouncingBall.maxXCoordinate)+'px'; // Starting Position 
+bouncingBall.start = function() {
+  var ball = document.getElementById("ball");
+  ball.style.top = Math.random()*(bouncingBall.maxYCoordinate)+'px'; // Starting Position 
+  ball.style.left = Math.random()*(bouncingBall.maxXCoordinate)+'px'; // Starting Position 
 
   window.setInterval(bouncingBall.animateBall,100);
 }
 
-  bouncingBall.animateBall =function() {
+bouncingBall.animateBall =function() {
 
-    bouncingBall.xCooridinate = parseInt(ball.style.left);
-    bouncingBall.yCoordinate = parseInt(ball.style.top); 
-    bouncingBall.xCooridinate+= bouncingBall.addX;
-    bouncingBall.yCoordinate+= bouncingBall.addY;
-    bouncingBall.boundaryCondition();
-    bouncingBall.ball.style.left =bouncingBall.xCooridinate + "px";
-    bouncingBall.ball.style.top = bouncingBall.yCoordinate + "px";
+  bouncingBall.xCooridinate = parseInt(ball.style.left);
+  bouncingBall.yCoordinate = parseInt(ball.style.top); 
+  bouncingBall.xCooridinate+= bouncingBall.addX;
+  bouncingBall.yCoordinate+= bouncingBall.addY;
+  bouncingBall.boundaryCondition();
+  ball.style.left =bouncingBall.xCooridinate + "px";
+  ball.style.top = bouncingBall.yCoordinate + "px";
 }
 
 window.onload = bouncingBall.start;
