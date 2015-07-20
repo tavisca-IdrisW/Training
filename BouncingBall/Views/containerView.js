5dd54afba95361f9fff117367cfa14abda058df1
tavisca-idrisw <idrisw@tavisca.com>
2015-07-20 20:09:40 +0530

    Suggested Changes made.

---
+++ BouncingBall/Views/containerView.js
@@ -0,0 +1,16 @@
+window.bouncingBall = window.bouncingBall || {};
+
+  bouncingBall.boundaryCondition = function()
+  {
+    bouncingBall.maxXCoordinate = window.innerWidth-40;
+    bouncingBall.maxYCoordinate = window.innerHeight-45;
+     if(bouncingBall.xCooridinate > bouncingBall.maxXCoordinate || 
+      bouncingBall.xCooridinate < bouncingBall.minXCoordinate) {
+      bouncingBall.addX =- bouncingBall.addX;
+     }
+      
+    if(bouncingBall.yCoordinate > bouncingBall.maxYCoordinate || 
+      bouncingBall.yCoordinate < bouncingBall.minYCoordinate) {
+      bouncingBall.addY =- bouncingBall.addY;
+    }
+  }
