c070274e4572bfdc57e48a2ce7aeaf8fb70bfd37
tavisca-idrisw <idrisw@tavisca.com>
2015-07-21 16:41:22 +0530

    Bouncing Ball changes made.

--- BouncingBall/Views/ballView.js
+++ BouncingBall/Views/ballView.js
@@ -1,29 +1,50 @@
-window.bouncingBall = window.bouncingBall || {
- minXCoordinate: 15,
- maxXCoordinate: window.innerWidth-40,
- minYCoordinate: 15,
- maxYCoordinate: window.innerHeight-45,
- addX: 20,
- addY: 15
-}
+"use strict";
 
-bouncingBall.start = function() {
-  var ball = document.getElementById("ball");
-  ball.style.top = Math.random()*(bouncingBall.maxYCoordinate)+'px'; // Starting Position 
-  ball.style.left = Math.random()*(bouncingBall.maxXCoordinate)+'px'; // Starting Position 
+window.bouncingBall = window.bouncingBall || {};
 
-  window.setInterval(bouncingBall.animateBall,100);
-}
+window.bouncingBall.bBall = function(elementID, x, y) {
+  
+  var ball = document.getElementById(elementID),
+      xCooridinate =  x,
+      yCoordinate = y,
+      addX = 15,
+      addY = 20;
+  
+  return {
+    ball: ball,
+    xCooridinate: xCooridinate,
+    yCoordinate: yCoordinate,
+    animateBall: function() {
 
-bouncingBall.animateBall =function() {
+      var containerObj = window.bouncingBall.containerView();
+      var changeCoordinate = containerObj.bondaryCheck(xCooridinate, yCoordinate);
 
-  bouncingBall.xCooridinate = parseInt(ball.style.left);
-  bouncingBall.yCoordinate = parseInt(ball.style.top); 
-  bouncingBall.xCooridinate+= bouncingBall.addX;
-  bouncingBall.yCoordinate+= bouncingBall.addY;
-  bouncingBall.boundaryCondition();
-  ball.style.left =bouncingBall.xCooridinate + "px";
-  ball.style.top = bouncingBall.yCoordinate + "px";
-}
+      //To check if X-limit(width) has been reached....
+      if (changeCoordinate.indexOf("changeX") > -1){
+        addX = -addX;
+      }
 
-window.onload = bouncingBall.start;
+      //To check if Y-limit(height) has been reached....
+      if (changeCoordinate.indexOf("changeY") > -1){
+        addY = -addY;
+      }
+
+      xCooridinate+= addX;
+      yCoordinate+= addY;
+
+      ball.style.left = xCooridinate + "px";
+      ball.style.top = yCoordinate + "px";
+    }
+  };
+};
+
+ var start = function() {
+  var initialContainer = window.bouncingBall.containerView();
+  var newBall = window.bouncingBall.bBall("ball", 
+    Math.random()*(initialContainer.maxXCoordinate),
+    Math.random()*(initialContainer.maxYCoordinate));
+  
+  window.setInterval(newBall.animateBall,100);
+};
+
+window.onload = start;
--- BouncingBall/Views/containerView.js
+++ BouncingBall/Views/containerView.js
@@ -1,16 +1,27 @@
+"use strict";
+
 window.bouncingBall = window.bouncingBall || {};
 
-bouncingBall.boundaryCondition = function()
-{
-  bouncingBall.maxXCoordinate = window.innerWidth-40;
-  bouncingBall.maxYCoordinate = window.innerHeight-45;
-   if(bouncingBall.xCooridinate > bouncingBall.maxXCoordinate || 
-    bouncingBall.xCooridinate < bouncingBall.minXCoordinate) {
-    bouncingBall.addX =- bouncingBall.addX;
-   }
+window.bouncingBall.containerView = function() {
+  var maxXCoordinate = window.innerWidth-40;
+  var maxYCoordinate = window.innerHeight-45;
+  var minXCoordinate = 15;
+  var minYCoordinate = 15;
+
+  return {
+    maxXCoordinate: maxXCoordinate,
+    maxYCoordinate: maxYCoordinate,
     
-  if(bouncingBall.yCoordinate > bouncingBall.maxYCoordinate || 
-    bouncingBall.yCoordinate < bouncingBall.minYCoordinate) {
-    bouncingBall.addY =- bouncingBall.addY;
-  }
-}
+    bondaryCheck: function(x, y) {
+      var check = "";
+      if(x > maxXCoordinate || x < minXCoordinate) {
+        check = check + "changeX";
+      }
+        
+      if(y > maxYCoordinate || y < minYCoordinate) {
+        check = check + "changeY";
+      }
+      return check;
+    }
+  };
+};
